9c8a81b2736ace221469aa13caa04a083afdf646
mathias labora martirena <mathiaslabora@gmail.com>
2022-03-30 14:20:02 -0300

    creando estructura

---
+++ src/main/java/com/example/demo/repository/VolantesRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VolantesRepository extends ReactiveMongoRepository {
+
+}
